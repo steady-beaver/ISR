e0df52160449b00029bad0d989b44b8abcbc79c9
ivo-rsk <ip@risika.dk>
2024-03-12 17:17:54 +0200

    StandardHeaderSection responsive

--- src/pages/services.js
+++ src/pages/services.js
@@ -2,13 +2,17 @@ import Container from 'components/Container';
 import Layout from 'components/Layout';
 import NewContainer from 'components/NewContainer/NewContainer';
 import Section from 'components/Section';
-import TestimonialSection from 'scenes/TestimonialSection/TestimonialSection';
+import StandardHeaderSection from 'scenes/StandardHeaderSection/StandardHeaderSection';
 
 export default function Home() {
   return (
     <Layout>
       <NewContainer>
-        <TestimonialSection />
+        <StandardHeaderSection
+          subtitle="Services"
+          title="For you"
+          text="Lorem ipsum dolor sit amet consectetur. Urna convallis leo scelerisque netus. Odio leo turpis non nunc ornare varius fusce et. "
+        />
       </NewContainer>
 
       <Section>
