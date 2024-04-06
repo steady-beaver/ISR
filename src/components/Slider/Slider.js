81ff605a103695f52be705712845c81b77b622c8
ivo-rsk <ip@risika.dk>
2024-04-06 11:54:50 +0300

    F

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,7 +2,7 @@ import styles from './Slider.module.scss';
 
 const Slider = ({ photoUrl, topText, title, bottomText, className }) => {
   return (
-    <div className={`${styles.main} ${className} `} style={{ backgroundImage: `url(${photoUrl})` }}>
+    <div className={`${styles.main}  ${className} `} style={{ backgroundImage: `url(${photoUrl})` }}>
       <span className={`${styles.upperText} subtitle letterSpacing `}>{topText}</span>
       <h2 className={`${styles.title} `}>{title}</h2>
       <p className={`${styles.bottomText} text `}>{bottomText}</p>
