d79b0c878e79552b20ea6a24e44096ddaa60f7a6
steady-beaver <ivaylo.yordanov.petrov@gmail.com>
2024-04-13 05:51:23 +0300

    About me static

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import Layout from 'components/Layout';
 import NewContainer from 'components/NewContainer/NewContainer';
 import AllThatMattersSection from 'scenes/AllThatMattersSection/AllThatMattersSection';
+import OldSchoolTimeline from 'scenes/OldSchoolTimeline/OldSchoolTimeline';
 import PersonalSection from 'scenes/PersonalSection/PersonalSection';
 import QuatroSectionVarB from 'scenes/QuatroSectionVarB/QuatroSectionVarB';
 import StandardHeaderSection from 'scenes/StandardHeaderSection/StandardHeaderSection';
-import Episode from 'scenes/TimelineSection/Episode';
-import TimelineSection from 'scenes/TimelineSection/TimelineSection';
+// import Episode from 'scenes/TimelineSection/Episode';
+// import TimelineSection from 'scenes/TimelineSection/TimelineSection';
 import VideoSection from 'scenes/VideoSection/VideoSection';
 
 export default function Home() {
@@ -17,21 +18,16 @@ export default function Home() {
           title="Hi, I'm Ola"
           text="A photographer who adores coffee, traveling, nature, sunsets, draws inspiration from music, loves yoga, and can spend hours gazing at the ocean."
         />
-      </NewContainer>
 
-      <NewContainer>
         <VideoSection url="https://www.youtube.com/watch?v=JtYaDKNssbI" />
-      </NewContainer>
 
-      <NewContainer>
-        <TimelineSection>
+        {/* <TimelineSection>
           <Episode
             image="/pictures/about-me/about-me-1.jpg"
             year="2002"
             text="I invested all my savings in a Mamiya analog camera. My dad, a photography enthusiast himself, passed down his knowledge of camera settings to me. Since then, I've been spending all my money on film rolls and setting up a darkroom under my desk."
           />
           <Episode
-            image="/pictures/about-me/about-me-2.jpg"
             year="2007"
             text="Dropping out of my tourism studies to apply to a photography school on the same day, I've been immersed in the world of photography ever since. Meeting beautiful people along the way, I started working as a photography teacher a year later."
           />
@@ -45,10 +41,10 @@ export default function Home() {
             year="2018"
             text="Exactly one year later, with our car packed to the ceiling, we left Poland and moved to Portugal. I left my photography studio behind in Poland to start afresh. Since then, I've been immersed in wedding photography in Portugal, and I wouldn't have it any other way."
           />
-        </TimelineSection>
-      </NewContainer>
+        </TimelineSection> */}
 
-      <NewContainer>
+        <OldSchoolTimeline />
+
         <QuatroSectionVarB
           header="Portugal"
           title="Lorem ipsum dolor sit amet consectetur. Urna convallis leo scelerisque netus."
